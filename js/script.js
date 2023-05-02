dea7085e29b78c7f0506eca9f39cbdf53d12c64c
Ariel Calipay <83052833+arielcalipay20@users.noreply.github.com>
2023-05-02 22:09:59 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const downloadBtn = document.getElementById('download-btn');
+const apkUrl = 'apk/app-universal-release.apk';
+
+downloadBtn.addEventListener('click', () => {
+    window.location.href = apkUrl;
+});
+
+const downloadBtn1 = document.getElementById('download-btn1');
+const apkUrl1 = 'apk/app-universal-release.apk';
+
+downloadBtn1.addEventListener('click', () => {
+    window.location.href = apkUrl1;
+});
